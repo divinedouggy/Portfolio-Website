92dd31953ebb150c2fb1472da9ffc2bd3124e6b6
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-11-26 19:37:46 -0500

    lots of changes, project pics, css, react magic

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,36 +1,133 @@
 import reactLogo from '../Images/react.png';
+import htmlLogo from '../Images/html.png';
+import cssLogo from '../Images/css.png'
+import javascriptLogo from '../Images/javascript.png'
+import expressLogo from '../Images/express.png'
+import mongoLogo from '../Images/mongodb.png'
+import mySQLLogo from '../Images/mysql.png'
+import nodeLogo from '../Images/node.png'
+import movieBuffaloPic from '../Images/movie_buffalo.png'
+import movieBuffaloPic2 from '../Images/movie_buffalo_2.png'
+import movieBuffaloPic3 from '../Images/movie_buffalo_3.png'
+import whackAMolePic from '../Images/whackamole.png'
+import whackAMolePic2 from '../Images/whackamole_2.png'
+import whackAMolePic3 from '../Images/whackamole_3.png'
+import youZooPic from '../Images/youzoo.png'
+import youZooPic2 from '../Images/youzoo_2.png'
+import youZooPic3 from '../Images/youzoo_3.png'
+import specialIngredientsPic from '../Images/special_ingredients.png'
+import specialIngredientsPic2 from '../Images/special_ingredients_2.png'
+import specialIngredientsPic3 from '../Images/special_ingredients_3.png'
+
+import { useState, useEffect } from 'react';
 
 function Home() {
+    const [imageToDisplay, setImageToDisplay] = useState("")
+
+    
+    function getBigImage(event) {
+        setImageToDisplay(<img src={event.target.src} alt="screenshot"/>)
+    }
+
+    function closeImage() {
+        setImageToDisplay("")
+    }
+
+    
+ 
+   
+
     return (
         <div className="home-master-container">
-            <div><p>Hello, I'm Doug, a full-stack software developer and web designer.</p></div>
+
             <div className="home-container-top">
 
                 <div className="home-top-subcontainer1">
                     <div className="home-child-top1">
-                        <h1 style={{ fontSize: 45, marginRight:20 }}>Hello, </h1>
-                        <h1 style={{ fontSize: 55, marginRight:15  }}>I'm Doug.</h1>
+                        {/* <h1 style={{ fontSize: 45, marginRight:20 }}>Hello, </h1>
+                        <h1 style={{ fontSize: 55, marginRight:15  }}>I'm Doug.</h1> */}
 
+                        <img className='dev-icon' src={htmlLogo} />
                     </div>
 
                     <div className="home-child-top2">
-
-                        <h1>I am a</h1>
+                        <img className='dev-icon' src={cssLogo} />
+                        {/* <h1>I am a</h1>
                         <h1>full-stack</h1>
-                        <h1>software developer</h1>
+                        <h1>software developer</h1> */}
                     </div>
 
                 </div>
 
                 <div className="home-top-subcontainer2">
 
-                    <div className="home-child-top3">
+                    {/* <div className="home-child-top3"style={{ display:"none" }}>
                         <h1> and</h1>
                         <h1>web designer.</h1>
                     </div>
 
-                    <div className="home-child-top4">
+                    <div className="home-child-top4"style={{ display:"none" }}>
                         <h1>I have skills in HTML, CSS, Javascript, React, MongoDB, MySQL</h1>
+                    </div> */}
+                    <p>Hello, I'm Doug. <br/>
+                    I am a full-stack software developer</p>
+                </div>
+            </div>
+
+            <div className="home-container-bottom">
+                <div className="home-bottom-subcontainer1">
+                    <div className="home-child-bottom1">
+                        {/* <p>This is my website</p> */}
+                        <img className='javascript-icon' src={javascriptLogo} />
+
+                    </div>
+                    <div className="home-child-bottom2">
+                        {/* <h3>Weee</h3> */}
+                        <img className='react-icon' src={reactLogo} />
+                    </div>
+
+                </div>
+
+                <div className="home-bottom-subcontainer2">
+                    {/* <div className="home-child-bottom3"style={{ display:"none" }}>
+                        <p>This is my website</p>
+                    </div>
+                    <div className="home-child-bottom4"style={{ display:"none" }}>
+                        <h3>Weee</h3>
+                        <img className='dev-icon' src={reactLogo} />
+                    </div> */}
+                    <p>and web designer.</p>
+                </div>
+            </div>
+            <div className="home-container-top">
+
+                <div className="home-top-subcontainer1">
+                    {/* <div className="home-child-top1" style={{ display:"none" }}>
+                        <h1 >Hello, </h1>
+                        <h1 style={{ fontSize: 55, marginRight:15  }}>I'm Doug.</h1>
+
+                    </div>
+
+                    <div className="home-child-top2"style={{ display:"none" }}>
+
+                        <h1>I am a</h1>
+                        <h1>full-stack</h1>
+                        <h1>software developer</h1>
+                    </div> */}
+               <p>I have graduated from Per Scholas</p>
+                </div>
+
+                <div className="home-top-subcontainer2">
+
+                    <div className="home-child-top3">
+                        {/* <h1> and</h1>
+                        <h1>web designer.</h1> */}
+                        <img className='express-icon' src={expressLogo} />
+                    </div>
+
+                    <div className="home-child-top4">
+                        <img className='mongodb-icon' src={mongoLogo} />
+                        {/* <h1>I have skills in HTML, CSS, Javascript, React, MongoDB, MySQL</h1> */}
                     </div>
 
                 </div>
@@ -38,26 +135,61 @@ function Home() {
 
             <div className="home-container-bottom">
                 <div className="home-bottom-subcontainer1">
-                    <div className="home-child-bottom1">
+                    {/* <div className="home-child-bottom1"style={{ display:"none" }}>
                         <p>This is my website</p>
                     </div>
-                    <div className="home-child-bottom2">
+                    <div className="home-child-bottom2"style={{ display:"none" }}>
                         <h3>Weee</h3>
-                    </div>
-
+                    </div> */}
+                    <p>Software Engineering intensive</p>
                 </div>
 
                 <div className="home-bottom-subcontainer2">
                     <div className="home-child-bottom3">
-                        <p>This is my website</p>
+                        <img className='mysql-icon' src={mySQLLogo} />
+                        {/* <p>This is my website</p> */}
                     </div>
                     <div className="home-child-bottom4">
-                        <h3>Weee</h3>
-                        <img className='dev-icon' src={reactLogo} />
+                        <img className='node-icon' src={nodeLogo} />
+                        {/* <h3>Weee</h3> */}
+
                     </div>
                 </div>
             </div>
+            <p className='projects'>Have a look at my projects. <br/><br/></p>
+
+
+            <div className="screenshot" onClick={closeImage}>{imageToDisplay}</div>
+
+            <div className='filmstrip'>
+            <p>&nbsp;&nbsp;&nbsp; Whack-A-Mole&nbsp;&nbsp;&nbsp;</p>
+                <img className='smallpic' onClick={getBigImage} src={whackAMolePic} />
+                <img className='smallpic' onClick={getBigImage} src={whackAMolePic2} />
+                <img className='smallpic' onClick={getBigImage} src={whackAMolePic3} />
+            </div>
+
+            <div className='filmstrip'>
+            &nbsp;
+                <img className='smallpic' onClick={getBigImage} src={movieBuffaloPic} />
+                <img className='smallpic' onClick={getBigImage} src={movieBuffaloPic2} />
+                <img className='smallpic' onClick={getBigImage} src={movieBuffaloPic3} /> <p>&nbsp;&nbsp;&nbsp;MOVIE BUFFalo</p>
+            </div>
+            
+            <div className='filmstrip'>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YouZoo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <img className='smallpic' onClick={getBigImage} src={youZooPic} />
+                <img className='smallpic' onClick={getBigImage} src={youZooPic2} />
+                <img className='smallpic' onClick={getBigImage} src={youZooPic3} />
+            </div>
+
+            <div className='filmstrip'>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <img className='smallpic' onClick={getBigImage} src={specialIngredientsPic} />
+                <img className='smallpic' onClick={getBigImage} src={specialIngredientsPic2} />
+                <img className='smallpic' onClick={getBigImage} src={specialIngredientsPic3} /> <p>&nbsp;&nbsp;&nbsp;Special Ingredients</p>
+            </div>
         </div>
+      
     )
 }
 
---
+++ src/Components/ToDo.js
@@ -0,0 +1,73 @@
+    /*~*    Overall/Mastering    *~*/ 
+    // Looking nice is one thing.... make it *~POP~*
+
+
+    /*~*    Circles and Squares    *~*/ 
+    // minor tweaks
+        // perhaps add/mimic 3D projection effect to Circle/Squares, OR
+        // just add a nice shadow for the whole shape
+    // bonus
+        // Maybe add another ShapePanel for Postman, APIs, anything else?
+    //
+
+
+    /*~*    User Experience    *~*/ 
+    // links in Header scroll down to respective part of single-page-application
+    // animation as user scrolls down... animation rewinds when user scrolls up
+
+
+    /*~*    Projects Section    *~*/ 
+    // cursor idea
+        // for small images, make cursor an "expand" symbol (like 4 arrows in an X shape)
+        // for large imgage, make cursor an X
+
+    // big image
+        // make pictures whose title appears over them upon hover (best to always have them there on mobile)
+        // make an X appear on hover, on big image
+        // make a transition Fade/Shrink when closing an image
+    // small images
+        // add hover effect of some sort
+        // add shadows or background color, something to glue images together more
+    //
+
+
+    /*~*    Contact Section    *~*/ 
+    // Github link
+    // LinkedIn link
+    // Email form? (May be best for security)
+    // Resume
+
+
+    /*~*    Personalize    *~*/ 
+    // add devicons for other interests, representing:
+        // nature (tree)
+        // music (music note)
+        // dogs (paw print)
+        // reading (book)
+        // video games (nes controller)
+    // include picture of myself somewhere (need to figure out what I'm comfortable/confident with)
+
+
+
+
+
+
+///////////////////////// DONE /////////////////////////////
+
+
+
+       /*~*    Circles and Squares    *~*/ 
+    // hover
+        // add hover properties to Squares
+        // change the Circle hover properties to be non-invasive
+    // minor tweaks
+        // round out all the Square corners, or none of them
+
+
+
+
+            /*~*    Projects Section    *~*/ 
+    // getting started
+        // Add pictures
+    // advanced
+        // make pictures pop up in dedicated window when clicked, like Special Ingredients
